47eec7d3232bf67843c0bd8a10d9f600383e2748
shashank-dewangan <shashank.at30@gmail.com>
2022-05-22 18:43:35 +0530

    changed docker container

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,12 @@
-import express from "express";
-import { json } from "body-parser";
+import express from 'express';
+import { json } from 'body-parser';
 
 const app = express();
 app.use(json());
 
+app.get('/api/users/getUser', (req, res) => {
+  res.send('<h1>Hello</h1>');
+});
 app.listen(3000, () => {
-  console.log("Listening on port 3000!");
+  console.log('Listening on port 3000!!');
 });
