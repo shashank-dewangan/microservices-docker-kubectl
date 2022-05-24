a378b6cf951a60842a3c0b398a07eeb7e3ba2fd0
shashank-dewangan <shashank.at30@gmail.com>
2022-05-24 18:15:21 +0530

    added ingress service yaml

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import { json } from 'body-parser';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 app.use(json());
 
 app.get('/api/users/getUser', (req, res) => {
   res.send('<h1>Hello</h1>');
 });
 app.listen(3000, () => {
-  console.log('Listening on port 3000!!');
+  console.log('Listening on port 3000!');
 });
